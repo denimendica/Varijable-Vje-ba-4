8353f9ac476e97fdcb9ea53eaa24fd0f8558505a
denimendica <71275614+denimendica@users.noreply.github.com>
2020-11-22 12:35:28 +0100

    Add files via upload

---
+++ varijableVježba4/varijableVježba4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varijableVježba4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sVelika, sMala, sPrvaTri, sZadnjihPet, s8_11;
+
+            Console.Write("Unesite niz znakova: ");
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
